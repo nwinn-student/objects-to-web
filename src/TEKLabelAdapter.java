665b1ce5e88bc80e8c9bf80e122c46c826dbafda
MaraDoze <122954800+MaraDoze@users.noreply.github.com>
2024-10-02 17:43:45 -0700

    Update TEKLabelAdapter.java (view/edit)
    right click on object to open edit window added

--- src/TEKLabelAdapter.java
+++ src/TEKLabelAdapter.java
@@ -2,19 +2,29 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.Component;
 import java.awt.Cursor;
+import java.awt.event.MouseAdapter;
+import javax.swing.SwingUtilities;
 /**
  * Houses TEKLabel's listeners, whatever they may be in time, 
  * utilizing mouse events primarily.
  *
  * @see java.awt.event.MouseEvent
  * @author Hayden Verstrat, Noah Winn
- * @version Sept. 30, 2024
+ * @version Oct. 2, 2024
  */
 public class TEKLabelAdapter implements MouseListener{
     /**
      * Selects and deselects the TEKLabel
      */
     public void mouseClicked(MouseEvent e){
+        //right-click opens the Edit View
+        if (SwingUtilities.isRightMouseButton(e)) {
+            TEKLabel label = (TEKLabel) e.getSource();
+            ObjectUI obj = label.getObjectFromText(); // Retrieve object from the label
+            if (obj != null) {
+                new TEKEditView(obj);  // Open Edit View for the object
+            }
+        }
         // Hayden here
         
     }
