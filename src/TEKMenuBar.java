b0009446a861a50f8f433e308d8ceb89acc4dd74
Zysteriya <143790465+Zysteriya@users.noreply.github.com>
2024-10-09 19:51:18 -0400

    Update TEKMenuBar.java

--- src/TEKMenuBar.java
+++ src/TEKMenuBar.java
@@ -58,10 +58,6 @@ public class TEKMenuBar extends JMenuBar{
         addMenuItem("Zoom Out", viewMenu, KeyEvent.VK_MINUS, "Use Ctrl-- to zoom out.");
         // cont. adding more
         
-        // View   NOTE: ctrl + scrollup and ctrl + scrolldown work MUCH better than ctrl-+ and ctrl--. Don't know how to visualize those will fix later.
-        addMenuItem("Zoom In", viewMenu, KeyEvent.VK_PLUS, "Use Ctrl-+ to zoom in.");
-        addMenuItem("Zoom Out", viewMenu, KeyEvent.VK_MINUS, "Use Ctrl-- to zoom out.");
-        
     }
     /**
      * For telling the object which items you wish to not require the user
