2929e6ab0f5d6dce7ee4be9c5bb58aa24fea903b
coby0815 <143664305+coby0815@users.noreply.github.com>
2024-10-29 21:17:51 -0400

    Update TEKLabelAdapter.java
    Include Object highlighting feature in this version

--- src/tekgui/adapter/TEKLabelAdapter.java
+++ src/tekgui/adapter/TEKLabelAdapter.java
@@ -6,6 +6,7 @@ import tekgui.window.TEKLabel;
 import tekgui.TEKFile;
 
 // Java imports
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.Component;
@@ -16,8 +17,8 @@ import javax.swing.JComponent;
  * Houses TEKLabel's listeners, whatever they may be in time, utilizing mouse events primarily.
  *
  * @see java.awt.event.MouseEvent
- * @author Hayden Verstrat, Noah Winn
- * @version Oct. 13, 2024
+ * @author Hayden Verstrat, Noah Winn, Coby Zhong
+ * @last edited at 10/29/2024
  */
 public class TEKLabelAdapter implements MouseListener{
     TEKPanel pan = null;
@@ -40,14 +41,32 @@ public class TEKLabelAdapter implements MouseListener{
      * Upon hover, the cursor is adjusted to the drag cursor to signify that an action can be done.
      */
     public void mouseEntered(MouseEvent e){
-        ((Component)e.getSource()).setCursor(new Cursor(Cursor.HAND_CURSOR));
-    }
     /**
+     * ((Component)e.getSource()).setCursor(new Cursor(Cursor.HAND_CURSOR));
      * Upon exiting the component, the cursor is reset to default.
      */
-    public void mouseExited(MouseEvent e){
-        ((Component)e.getSource()).setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+         Component component = (Component) e.getSource();
+         component.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        
+    if (component instanceof JComponent) {
+            ((JComponent) component).setBackground(Color.LIGHT_GRAY); 
+            ((JComponent) component).setOpaque(true); 
+        }
     }
+    public void mouseExited(MouseEvent e){
+    /**
+     * ((Component)e.getSource()).setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+     */
+
+        Component component = (Component) e.getSource();
+        component.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+        
+        if (component instanceof JComponent) {
+            ((JComponent) component).setBackground(null); 
+            ((JComponent) component).setOpaque(false);
+        }
+    }
+    
     public void mousePressed(MouseEvent e){
         
     }
