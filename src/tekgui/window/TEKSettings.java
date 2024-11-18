9c36fc1e89e87831bc24c4255dcd38f2c867417a
MaraDoze <122954800+MaraDoze@users.noreply.github.com>
2024-11-18 06:17:07 -0800

    Update TEKSettings.java integration/continuation
    added setting options (theme, font size, and height/width)

--- src/tekgui/window/TEKSettings.java
+++ src/tekgui/window/TEKSettings.java
@@ -16,7 +16,7 @@ import javax.swing.JButton;
  * Write a description of class TEKSettings here.
  *
  * @author Mara Doze, Zakariya Javed, Noah Winn
- * @version November 11, 2024
+ * @version November 18, 2024
  */
 public class TEKSettings extends JDialog{
     public TEKSettings(JFrame parent){
@@ -26,7 +26,31 @@ public class TEKSettings extends JDialog{
         JPanel container = new JPanel();
         container.setPreferredSize(new Dimension(300,300));
         
-            // Labels and Buttons can go here
+        // settings options in the container panel
+        JLabel themeLabel = new JLabel("Select Theme:");
+        String[] themes = {"Light", "Dark"}; //theme options
+        JComboBox<String> themeComboBox = new JComboBox<>(themes);
+        
+        JLabel fontLabel = new JLabel("Select Font Size:");
+        String[] fontSizes = {"Small", "Medium", "Large"}; //font size options
+        JComboBox<String> fontSizeComboBox = new JComboBox<>(fontSizes);
+
+        // the components for the window size adjustment
+        JLabel widthLabel = new JLabel("Window Width:");
+        JTextField widthField = new JTextField("500", 10); // Default width
+        JLabel heightLabel = new JLabel("Window Height:");
+        JTextField heightField = new JTextField("300", 10); // Default height
+
+        // components to the container panel
+        container.setLayout(new GridLayout(4, 2));
+        container.add(themeLabel);
+        container.add(themeComboBox);
+        container.add(fontLabel);
+        container.add(fontSizeComboBox);
+        container.add(widthLabel);
+        container.add(widthField);
+        container.add(heightLabel);
+        container.add(heightField);
         
         JTabbedPane pane = new JTabbedPane();
         pane.add("Container", container);
@@ -35,6 +59,20 @@ public class TEKSettings extends JDialog{
         JButton confirm = ButtonBuilder.addButton("Confirm", exitHouse, "Button to submit changes of settings.", new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // parent.updateSettings(...) // send changes to parent
+                dispose();
+            }
+        });
+
+        JButton cancel = ButtonBuilder.addButton("Cancel", exitHouse, "Button to cancel changes to settings.", new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Save settings 
+                String selectedTheme = (String) themeComboBox.getSelectedItem();
+                String selectedFontSize = (String) fontSizeComboBox.getSelectedItem();
+                int width = Integer.parseInt(widthField.getText());
+                int height = Integer.parseInt(heightField.getText());
+
+                // Calling the parent method to update settings
+                ((MainWindow) parent).updateSettings(selectedTheme, selectedFontSize, width, height);
                 dispose();
             }
         });
